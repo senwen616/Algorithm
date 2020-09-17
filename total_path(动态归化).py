715800b416b9a473b78926c944104e66047a0e70
sunwenping <sunwenping@qianxin.com>
2020-09-17 11:29:41 +0800

    Add 路径总和，动态规划示例1

---
+++ total_path(动态归化).py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@author: swp
+@file: total_pah(动态归化).py
+@time:   2020/9/17
+@version:  
+"""
+"""
+描述：从上至下，从左往右，走到最后一格的路径总条数
+----------
+----------
+----------
+----------
+"""
+
+
+# 利用动态规划，当前格子的路径数来自它的左边和上边
+
+def get_total_path(m, n):
+    nums = [1 for _ in range(n)]
+    for i in range(m-1):
+        for j in range(n):
+            if j == 0:
+                nums[j] = 1
+            else:
+                nums[j] = nums[j - 1] + nums[j]
+    return nums
+
+
+if __name__ == '__main__':
+    print(get_total_path(3, 2))
